0d1d1e9df32a1f10122d5b6ad7bdf98641b2e829
SimonTry <simon.sanchez>
2025-10-02 07:36:26 -0500

    (feat): se agrega lógica para todas las operaciones CRUD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import org.example.Model.Estudiante;
 import org.example.Services.EstudianteService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
@@ -16,7 +20,52 @@ public class Main {
     @Bean
     public CommandLineRunner run(EstudianteService estudianteService) {
         return args -> {
-            System.out.println("--- DEMOSTRACIÓN CRUD (JDK 21) ---");
+            System.out.println("-- Bienvenido al Sistema --");
+
+            System.out.println("Listado de estudiantes");
+            List<Estudiante> estudiantes = estudianteService.getAllStudents();
+
+            estudiantes.forEach(System.out::println);
+
+            /*System.out.println("Ingrese los datos de un estudiante: ");
+            Estudiante objEstudiante = new Estudiante("Pepito",
+                    "Perez",
+                    "perez@gmail.com");
+
+            Estudiante estudianteRegistrado = estudianteService.saveStudent(objEstudiante);
+
+            System.out.println("Listado de estudiantes");
+            estudiantes = estudianteService.getAllStudents();
+
+            estudiantes.forEach(System.out::println);*/
+
+            System.out.println("Traer estudiante por id");
+            Long id_a_consultar = 5L;
+
+            estudianteService.getStudentById(id_a_consultar).ifPresentOrElse(
+                    System.out::println,
+                    () -> System.out.println("No se encontró el usuario")
+            );
+
+
+            Estudiante informacionParaActualizar = new Estudiante("Pedro", "Pascal", "pascalito@gmail.com");
+            Estudiante informacionYaActualizada = estudianteService.updateStudenInfo(id_a_consultar,informacionParaActualizar);
+
+            System.out.println("Estudiante actualizado: "+informacionYaActualizada);
+
+
+            System.out.println("Eliminar el estudiante con id 7");
+
+            boolean resultado;
+
+            resultado = estudianteService.deleteStudentInfo(7L);
+
+            if(resultado){
+                System.out.println("Se ha eliminado el estudiante");
+            }else{
+                System.out.println("No se pudo eliminar el estudiante");
+            }
+
 
         };
     }
--- src/main/java/org/example/Services/EstudianteService.java
+++ src/main/java/org/example/Services/EstudianteService.java
@@ -6,20 +6,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+// INCORPORAR TODA LA LÓGICA CRUD
 @Service
 public class EstudianteService {
 
     @Autowired
     private EstudianteRepository estudianteRepository;
 
-    public Estudiante crearEstudiante(Estudiante estudiante) {
-        return estudianteRepository.save(estudiante);
-    }
-
-
-
     public List<Estudiante> getAllStudents() {
         return estudianteRepository.findAll();
     }
+
+    public Estudiante saveStudent(Estudiante estudiante){
+        return estudianteRepository.save(estudiante);
+    }
+
+    public Optional<Estudiante> getStudentById(Long Id){
+        return estudianteRepository.findById(Id);
+    }
+
+
+    public Estudiante updateStudenInfo(Long Id, Estudiante nuevaInfoEstudiante){
+        return estudianteRepository.findById(Id).map(
+                estudiante ->{
+                    estudiante.setNombre(nuevaInfoEstudiante.getNombre());
+                    estudiante.setApellido(nuevaInfoEstudiante.getApellido());
+                    estudiante.setEmail(nuevaInfoEstudiante.getEmail());
+                    return estudianteRepository.save(estudiante);
+                }
+        ).orElse(null);
+    }
+
+
+    public boolean deleteStudentInfo(Long Id){
+        if(estudianteRepository.existsById(Id)){
+            estudianteRepository.deleteById(Id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
